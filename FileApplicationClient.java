efdaf0b593187d2f67d0fe2e82a3107fc629940d
Opylo <optimartnetcom121@gmail.com>
2021-09-18 19:03:14 +0100

    added name to file

--- FileApplicationClient.java
+++ FileApplicationClient.java
@@ -2,6 +2,10 @@ package SimplilearnProjectFile;
 
 public class FileApplicationClient {
     public static void main(String[] args) {
+        System.out.println("***************************");
+        System.out.println("Application developed by Opeyemi Ariyo \n" +
+                "For project submission 18.09.2021");
+        System.out.println("***************************");
         FileApplication fileApplication = new FileApplication();
         fileApplication.fileApplication();
     }
